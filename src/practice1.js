1adb1d0309b46956ebaf1d45f422bd12c0518efd
xinyxie <xinyxie@ap.xom.com>
2020-06-30 19:03:00 +0800

    Finish practice1 and practice2.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
-export { parseData };
+const parseData = input => {
+  let {data, column} = input;
+
+  let columns = column.map(obj => obj.name);
+
+  return data.map(line => {
+    let result = {};
+    for (let [index, key] of columns.entries()) {
+      result[key] = line[index];
+    };
+    return result;
+  })
+};
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+
+  let itemAfter = sections.map(section => items[section.index]);
+
+  for (let [index, section] of sections.entries()) {
+    items.splice(items.indexOf(itemAfter[index]), 0, section.content);
+  }
+
+  return items;
+};
 export { inject };
